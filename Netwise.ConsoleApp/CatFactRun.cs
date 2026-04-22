316302cf0d9940223a18d66150fae9f120cb2dde
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-04-22 19:43:58 +0200

    Remove IConfiguration dependency from CatFactRun

--- Netwise.ConsoleApp/CatFactRun.cs
+++ Netwise.ConsoleApp/CatFactRun.cs
@@ -11,20 +11,16 @@ namespace Netwise.ConsoleApp
 {
     public sealed class CatFactRun : BackgroundService
     {
-        private readonly IConfiguration _configuration;
         private readonly ICatFactService _catFactService;
 
-        public CatFactRun(IConfiguration configuration, ICatFactService catFactService)
+        public CatFactRun(ICatFactService catFactService)
         {
-            _configuration = configuration;
             _catFactService = catFactService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var filePath = _configuration["FilePath"];
-            
-            await _catFactService.StartAsync(filePath,stoppingToken);
+            await _catFactService.StartAsync("catfacts.txt", stoppingToken);
         }
     }
 }
