9af9e6eecdb9cd67a4de74b9b2a0c6ca903da8c0
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-04-22 16:57:33 +0200

    Add CatFactService with ICatFactClien

---
+++ Netwise.Infrastructure/Services/CatFactService.cs
@@ -0,0 +1,25 @@
+﻿using Netwise.Domain.Interfaces.CatFact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netwise.Infrastructure.Services
+{
+    public sealed class CatFactService : ICatFactService
+    {
+        private readonly ICatFactClient _catFactClient;
+
+        public CatFactService(ICatFactClient catFactClient)
+        {
+            _catFactClient = catFactClient;
+        }
+
+        public Task StartAsync(string file, CancellationToken cancellationToken = default)
+        {
+            var fact = _catFactClient.GetFactAsync(cancellationToken); 
+            return Task.CompletedTask;
+        }
+    }
+}
