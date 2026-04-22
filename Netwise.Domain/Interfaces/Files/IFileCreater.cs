6075fce057f8be91220f7de49c26f81ed852ee6a
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-04-22 19:14:31 +0200

    Add IFileCreater interface and FileCreater implementation Add FileCreater implement file creation and writing

---
+++ Netwise.Domain/Interfaces/Files/IFileCreater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netwise.Domain.Interfaces.Files
+{
+    public interface IFileCreater
+    {
+        Task CreateContent(string file, string content, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Netwise.Infrastructure/Files/FileCreater.cs
@@ -0,0 +1,22 @@
+﻿using Netwise.Domain.Interfaces.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netwise.Infrastructure.Files
+{
+    public sealed class FileCreater : IFileCreater
+    {
+        public async Task CreateContent(string file, string content, CancellationToken cancellationToken = default)
+        {
+            var path = Path.GetDirectoryName(file);
+
+            await using var stream = File.Create(file);
+            await using var writer = new StreamWriter(stream);
+            await writer.WriteLineAsync(content);
+            await writer.FlushAsync(cancellationToken);
+        }
+    }
+}
