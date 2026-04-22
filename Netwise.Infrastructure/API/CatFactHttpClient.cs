726a5a0bff9d35526d9fcd746f2307285040db13
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-04-22 22:46:52 +0200

    Add logging and error handling to CatFactHttpClient

--- Netwise.Infrastructure/API/CatFactHttpClient.cs
+++ Netwise.Infrastructure/API/CatFactHttpClient.cs
@@ -1,9 +1,11 @@
-﻿using Netwise.Domain.Interfaces.CatFact;
+﻿using Microsoft.Extensions.Logging;
+using Netwise.Domain.Interfaces.CatFact;
 using Netwise.Domain.Models.CatFact;
 using Netwise.Infrastructure.API.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -13,21 +15,59 @@ namespace Netwise.Infrastructure.API
     public sealed class CatFactHttpClient : ICatFactClient
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger<CatFactHttpClient> _logger;
 
-        public CatFactHttpClient(HttpClient httpClient)
+        public CatFactHttpClient(HttpClient httpClient, ILogger<CatFactHttpClient> logger)
         {
             _httpClient = httpClient;
+            _logger = logger;
         }
 
         public async Task<CatFactModel> GetFactAsync(CancellationToken cancellationToken = default)
         {
-            using var res = await _httpClient.GetAsync("fact", cancellationToken);
+            try
+            {
+                using var res = await _httpClient.GetAsync("fact", cancellationToken);
 
-            var json = await res.Content.ReadAsStringAsync(cancellationToken);
-            var dto = JsonSerializer.Deserialize<CatFactApi>(json);
+                if (!res.IsSuccessStatusCode)
+                {
+                    _logger.LogError("API failed with {StatusCode}.", res.StatusCode);
+                    return null;
+                }
 
-            var length = dto.Length ?? dto.Fact.Length;
-            return new CatFactModel(dto.Fact, length);
+                var json = await res.Content.ReadAsStringAsync(cancellationToken);
+                var dto = JsonSerializer.Deserialize<CatFactApi>(json);
+
+                if (dto is null || string.IsNullOrWhiteSpace(dto.Fact))
+                {
+                    _logger.LogError("Data from API is null or empty");
+                    return null;
+                }
+
+                var length = dto.Length ?? dto.Fact.Length;
+                return new CatFactModel(dto.Fact, length);
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, "Network/API error fetch");
+                return null;
+            }
+            catch (TaskCanceledException ex)
+            {
+                _logger.LogError(ex, "Request timed out while fetch");
+                return null;
+            }
+            catch (JsonException ex)
+            {
+                _logger.LogError(ex, "Invalid JSON from API");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error.");
+                return null;
+            }
+
         }
     }
 }
