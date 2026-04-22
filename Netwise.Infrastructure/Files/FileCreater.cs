218aef0320a805df7a21b253fdddd11cd099fd60
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-04-22 22:58:15 +0200

    Add logging and error handling to FileCreater

--- Netwise.Infrastructure/Files/FileCreater.cs
+++ Netwise.Infrastructure/Files/FileCreater.cs
@@ -1,4 +1,5 @@
-﻿using Netwise.Domain.Interfaces.Files;
+﻿using Microsoft.Extensions.Logging;
+using Netwise.Domain.Interfaces.Files;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,39 @@ namespace Netwise.Infrastructure.Files
 {
     public sealed class FileCreater : IFileCreater
     {
+        private readonly ILogger<FileCreater> _logger;
+
+        public FileCreater(ILogger<FileCreater> logger)
+        {
+            _logger = logger;
+        }
         public async Task CreateContent(string file, string content, CancellationToken cancellationToken = default)
         {
-            var path = Path.GetDirectoryName(file);
+            try
+            {
+                var path = Path.GetDirectoryName(file);
 
-            await using var stream = new FileStream(file, FileMode.Append);
-            await using var writer = new StreamWriter(stream);
-            await writer.WriteLineAsync(content);
-            await writer.FlushAsync(cancellationToken);
+                if(!string.IsNullOrWhiteSpace(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                await using var stream = new FileStream(file, FileMode.Append);
+                await using var writer = new StreamWriter(stream);
+                await writer.WriteLineAsync(content);
+                await writer.FlushAsync(cancellationToken);
+            }
+            catch (IOException ex)
+            {
+                _logger.LogError(ex, "I/O error {file}", file);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "File error: {file}", file);
+                throw;
+            }
+            
         }
     }
 }
