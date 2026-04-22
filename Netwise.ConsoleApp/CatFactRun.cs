40fd7307b68a294ff84f3d920b366e281058af58
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-04-22 23:05:48 +0200

    Add logging and error handling to CatFactRun service

--- Netwise.ConsoleApp/CatFactRun.cs
+++ Netwise.ConsoleApp/CatFactRun.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Netwise.Domain.Interfaces.CatFact;
 using System;
 using System.Collections.Generic;
@@ -12,15 +13,26 @@ namespace Netwise.ConsoleApp
     public sealed class CatFactRun : BackgroundService
     {
         private readonly ICatFactService _catFactService;
+        private readonly ILogger<CatFactRun> _logger;
 
-        public CatFactRun(ICatFactService catFactService)
+        public CatFactRun(ICatFactService catFactService, ILogger<CatFactRun> logger)
         {
             _catFactService = catFactService;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await _catFactService.StartAsync("catfacts.txt", stoppingToken);
+            try
+            {
+                await _catFactService.StartAsync("catfacts.txt", stoppingToken);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while executing the cat fact service.");
+                
+            }
         }
     }
 }
